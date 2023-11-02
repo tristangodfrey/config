5e7675ecc754b10bb391c02576001a556cbc6976
Tristan Godfrey <tristan@godfrey.nl>
2023-11-02 17:56:35 +0100

    fix(cli): get proper parent directory in main.sh

--- packages/cli/scripts/main.sh
+++ packages/cli/scripts/main.sh
@@ -2,12 +2,11 @@
 
 # Check if 'bun' is available in the PATH
 
-PARENT_DIR="$(dirname "$(dirname "$0")")"
+PARENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 #If the last command was successful (exit status 0), use 'bun'
 if ! [ -x "$(command -v bun)" ]; then
   node "$PARENT_DIR/dist/main.js" "$@"
 else
   bun "$PARENT_DIR/src/main.ts" "$@"
-#  node "$PARENT_DIR/dist/main.js" "$@"
 fi
